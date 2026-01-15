a7108c352f751bdf913261a1e2750db1960e5aef
m <example@example.com>
2026-01-16 03:04:03 +0600

    Fix typos with _validate_input

--- COSC3339_Inclass_One.py
+++ COSC3339_Inclass_One.py
@@ -68,19 +68,18 @@ def curve_scores(scores):
 # For scenario three change the name of this method.
 # For scenario five fix the typos
 def _validate_input(text_value):
-
-    valud_imput = True 
+    valid_input = True 
     
     if text_value is None:
-        valud_imput = False
+        valid_input = False
     
     if text_value == "":
-        valud_imput = False
+        valid_input = False
         
-    return valud_imput
+    return valid_input
 
 def process_user_data(data):
-    return _validate_imput(data)
+    return _validate_input(data)
 
 def main():
     print("--- STARTING TESTS ---")
